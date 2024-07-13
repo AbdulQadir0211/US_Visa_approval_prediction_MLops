b49f361ab612b572267833b7707c711b74415fc8
AbdulQadir0211 <abdulkadir9929@gmail.com>
2024-07-14 02:31:30 +0530

    requirements add

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "us_visa",
+    version="0.1",
+    author="Abdul Qadir",
+    author_email= "abdulkadir9929@gmail.com",
+    packages = find_packages()
+)
